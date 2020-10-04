38672842dda1debbd5d1ba2875f675d00dfc3314
Sam Blum <samblum17@icloud.com>
2020-10-04 00:16:05 -0500

    Initial Commit

---
+++ Project Barb/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project Barb
+//
+//  Created by Sam Blum on 10/4/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project Barb/Project_BarbApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_BarbApp.swift
+//  Project Barb
+//
+//  Created by Sam Blum on 10/4/20.
+//
+
+import SwiftUI
+
+@main
+struct Project_BarbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
