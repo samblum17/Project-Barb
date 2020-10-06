68b316e7d18b9302ece1c3cc623434ded474b83a
Sam Blum <samblum17@icloud.com>
2020-10-06 00:47:28 -0500

    Set up small widget family design with hardcoded values

--- Project_BarbWidget/Project_BarbWidget.swift
+++ Project_BarbWidget/Project_BarbWidget.swift
@@ -13,15 +13,15 @@ struct Provider: IntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date(), configuration: ConfigurationIntent())
     }
-
+    
     func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
         let entry = SimpleEntry(date: Date(), configuration: configuration)
         completion(entry)
     }
-
+    
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
-
+        
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
@@ -29,7 +29,7 @@ struct Provider: IntentTimelineProvider {
             let entry = SimpleEntry(date: entryDate, configuration: configuration)
             entries.append(entry)
         }
-
+        
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
@@ -42,22 +42,28 @@ struct SimpleEntry: TimelineEntry {
 
 struct Project_BarbWidgetEntryView : View {
     var entry: Provider.Entry
-
+    
     var body: some View {
-        Text(entry.date, style: .time)
+        ZStack {
+            Color(.systemYellow).opacity(0.6)
+            Text("Good morning, Sam")
+                .font(.system(.title2, design: .default))
+                .bold()
+                .position(x: /*@START_MENU_TOKEN@*/85.0/*@END_MENU_TOKEN@*/, y: 120.0)
+        }
     }
 }
 
 @main
 struct Project_BarbWidget: Widget {
     let kind: String = "Project_BarbWidget"
-
+    
     var body: some WidgetConfiguration {
         IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
             Project_BarbWidgetEntryView(entry: entry)
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("Project Barb")
+        .description("Project Barb will implement a simple, personalized widget to warmly greet you, right on the home screen.")
     }
 }
 
